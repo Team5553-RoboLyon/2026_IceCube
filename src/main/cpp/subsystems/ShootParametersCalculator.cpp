76eddf293fe00de5c33090a397be28f0c9e48910
VictorT04 <victor.torres-tricard@robolyon.com>
2026-02-14 18:47:59 +0100

    Improve ShootParameterCalculator

--- src/main/cpp/subsystems/ShootParametersCalculator.cpp
+++ src/main/cpp/subsystems/ShootParametersCalculator.cpp
@@ -2,33 +2,74 @@
 
 #include "frc/DriverStation.h"
 #include "subsystems/turret/TurretConstants.h"
+#include "subsystems/shooter/hood/HoodConstants.h"
+#include "subsystems/shooter/flywheel/FlywheelConstants.h"
 #include "Constants.h"
 
-ShootParametersCalculator::ShootParametersCalculator(frc::Pose2d robotStartingPos)
-{
-    robotPos = robotStartingPos;
-}
+ShootParametersCalculator::ShootParametersCalculator()
+{}
 
 void ShootParametersCalculator::SetAlliance(frc::DriverStation::Alliance alliance)
 {
-    InBlueAlliance = (alliance == frc::DriverStation::Alliance::kBlue);
+    if (alliance == frc::DriverStation::Alliance::kBlue)
+        m_targetPos = FieldConstants::Hub::BLUE_PLACEMENT;
+    else
+        m_targetPos = FieldConstants::Hub::RED_PLACEMENT;
+
 }
 
-void ShootParametersCalculator::CalculateNewParameters(ShootParameters& params, frc::Pose2d newRobotPos)
-{ 
-    frc::Transform2d robotDisplacement = newRobotPos - robotPos;
+void ShootParametersCalculator::SetRobotPos(frc::Pose2d robotPos, double timestamp)
+{
+    m_lastRobotPos = robotPos;
+    m_lastTimestamp = timestamp;
+}
 
-    if (InBlueAlliance)
+void ShootParametersCalculator::CalculateNewParameters(ShootParameters& params, frc::Pose2d robotPos, double timestamp)
+{ 
+    frc::Transform2d robotDisplacement = robotPos - m_lastRobotPos;
+    double elapsedTime = m_lastTimestamp-timestamp;
+
+    double xSpeed = robotDisplacement.X().value()/(elapsedTime);
+    double ySpeed = robotDisplacement.Y().value()/(elapsedTime);
+    double rotationSpeed = robotDisplacement.Rotation().Radians().value()/(elapsedTime);
+
+    frc::Pose2d estimatedNextRobotPos = robotPos.Exp(frc::Twist2d{units::meter_t(xSpeed * TIME_PER_CYCLE), 
+                                                                  units::meter_t(ySpeed * TIME_PER_CYCLE),
+                                                                  units::radian_t(rotationSpeed * TIME_PER_CYCLE)});
+    frc::Pose2d estimatedNextTurretPos = estimatedNextRobotPos + TurretConstants::Specifications::ROBOT_TO_TURRET;
+
+    double nextTurretToHubDistance = (m_targetPos - estimatedNextTurretPos).Translation().Norm().value();
+    double timeToReachHub = m_timeToReachTargetMap[nextTurretToHubDistance];
+
+    frc::Pose2d lookAheadTargetPos = m_targetPos;
+    double lookAheadTargetDistance = nextTurretToHubDistance;
+    double timeToReachLookAheadTarget = timeToReachHub;
+
+    for (int i = 0; i < 20; i++) //TUNEME
     {
-        params.lookAheadTargetPos = FieldConstants::Hub::BLUE_PLACEMENT + robotDisplacement.Inverse();
-        params.lookAheadTargetTurretPos = WRAP_ANGLE_0_TO_2PI((FieldConstants::Hub::BLUE_PLACEMENT.Rotation().Radians() - newRobotPos.Rotation().Radians()).value());
+        timeToReachLookAheadTarget = m_timeToReachTargetMap[lookAheadTargetDistance];
+        lookAheadTargetPos = m_targetPos + frc::Transform2d {units::meter_t(-xSpeed*timeToReachLookAheadTarget), 
+                                                           units::meter_t(-ySpeed*timeToReachLookAheadTarget),
+                                                           0.0_rad};
+        lookAheadTargetDistance = (lookAheadTargetPos - estimatedNextTurretPos).Translation().Norm().value();
+    }
+
+    params.hoodPos = m_hoodPosMap[lookAheadTargetDistance];
+    if (params.hoodPos < HoodConstants::Position::MIN)
+    {
+        params.flywheelSpeed = FlywheelConstants::Speed::AGAINST_HUB;
+        params.hoodPos = HoodConstants::Position::MIN;
+    }
+    else if (params.hoodPos < HoodConstants::Position::MAX)
+    {
+        params.flywheelSpeed = FlywheelConstants::Speed::AGAINST_HUB;
+        params.hoodPos = HoodConstants::Position::MAX;
     }
     else
-    {
-        params.lookAheadTargetPos = FieldConstants::Hub::RED_PLACEMENT + robotDisplacement.Inverse();
-        params.lookAheadTargetTurretPos = WRAP_ANGLE_0_TO_2PI((FieldConstants::Hub::RED_PLACEMENT.Rotation().Radians() - newRobotPos.Rotation().Radians()).value());
-    }
+        params.flywheelSpeed = m_flywheelSpeedMap[lookAheadTargetDistance];
 
-    frc::Transform2d robotToLookAheadPos = (newRobotPos + TurretConstants::Specifications::ROBOT_TO_TURRET)-params.lookAheadTargetPos;
-    params.lookAheadTargetDistance = sqrt(pow(robotToLookAheadPos.X().value(),2)+pow(robotToLookAheadPos.Y().value(),2));
+    params.lookAheadTargetTurretPos = m_targetPos.Rotation().Radians().value() - estimatedNextTurretPos.Rotation().Radians().value();
+
+    m_lastRobotPos = robotPos;
+    m_lastTimestamp = timestamp;       
 }
--- src/main/include/subsystems/ShootParametersCalculator.h
+++ src/main/include/subsystems/ShootParametersCalculator.h
@@ -1,24 +1,31 @@
 #include "frc/geometry/Pose2d.h"
 #include "frc/DriverStation.h"
+#include <wpi/interpolating_map.h>
 
 struct ShootParameters {
     frc::Pose2d lookAheadTargetPos;
     double lookAheadTargetTurretPos = 0.0;
-    double lookAheadTargetDistance = 0.0;
+    double hoodPos = 0.0;
+    double flywheelSpeed = 0.0;
 };
 
 class ShootParametersCalculator
 {
     public:
 
-     ShootParametersCalculator(frc::Pose2d robotStartingPos);
+     ShootParametersCalculator();
      ~ShootParametersCalculator() = default;
 
      void SetAlliance(frc::DriverStation::Alliance alliance);
-     void CalculateNewParameters(ShootParameters& params, frc::Pose2d robotPos);
+     void SetRobotPos(frc::Pose2d robotPos, double timestamp);
+     void CalculateNewParameters(ShootParameters& params, frc::Pose2d robotPos, double timestamp);
 
     private:
 
-     frc::Pose2d robotPos;
-     bool InBlueAlliance; //if not in blue alliance -> in red alliance
+     frc::Pose2d m_lastRobotPos;
+     frc::Pose2d m_targetPos;
+     double m_lastTimestamp = 0.0;
+     wpi::interpolating_map<double,double> m_hoodPosMap; //TODO : do tests to get at least 10 values for each interpolating map
+     wpi::interpolating_map<double,double> m_flywheelSpeedMap;
+     wpi::interpolating_map<double,double> m_timeToReachTargetMap;
 };
