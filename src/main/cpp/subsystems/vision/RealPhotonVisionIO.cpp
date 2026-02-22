e7687cf342a79cdb4c63d10ee01402d103d46758
adamKachmar <adam.kachmar@robolyon.com>
2026-02-22 14:20:36 +0100

    clear vision branch

--- src/main/cpp/subsystems/vision/RealPhotonVisionIO.cpp
+++
@@ -1,7 +0,0 @@
-#include "subsystems/vision/RealPhotonVisionIO.h"
-
-RealPhotonVisionIO::RealPhotonVisionIO(
-    const std::string& cameraName,
-    const frc::Transform3d& robotToCameraTransform,
-    const frc::AprilTagFieldLayout& aprilTagFieldLayout)
-    : PhotonVisionIO(cameraName, robotToCameraTransform, aprilTagFieldLayout) {}
