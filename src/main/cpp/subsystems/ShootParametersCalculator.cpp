ae5a620d5788e9a54fb084855b738a79acf38813
VictorT04 <victor.torres-tricard@robolyon.com>
2026-03-04 15:48:16 +0100

    Message (Ctrl+Enter to commit)

--- src/main/cpp/subsystems/ShootParametersCalculator.cpp
+++ src/main/cpp/subsystems/ShootParametersCalculator.cpp
@@ -87,8 +87,8 @@ void ShootParametersCalculator::CalculateHubNewParameters(ShootParameters& param
 
     double turretSuplAngleToAlignToTarget =  atan((lookAheadTargetPos-estimatedNextTurretPos).Y().value()/(lookAheadTargetPos-estimatedNextTurretPos).X().value()); //TODO : find a better name
 
-    params.lookAheadTargetTurretPos = m_hubTargetPos.Rotation().Radians().value() - estimatedNextTurretPos.Rotation().Radians().value()
-                                      + turretSuplAngleToAlignToTarget;
+    params.lookAheadTargetTurretPos = WRAP_ANGLE_0_TO_2PI(m_hubTargetPos.Rotation().Radians().value() - estimatedNextTurretPos.Rotation().Radians().value()
+                                      + turretSuplAngleToAlignToTarget);
 
     params.lookAheadTargetPos = lookAheadTargetPos;
 
@@ -140,7 +140,7 @@ void ShootParametersCalculator::CalculateAllianceZoneNewParameters(ShootParamete
     else
         params.flywheelSpeed = m_flywheelSpeedMap[lookAheadTargetDistance];
 
-    params.lookAheadTargetTurretPos = m_allianceZoneTargetPose.Rotation().Radians().value() - estimatedNextTurretPos.Rotation().Radians().value();
+    params.lookAheadTargetTurretPos = WRAP_ANGLE_0_TO_2PI(m_allianceZoneTargetPose.Rotation().Radians().value() - estimatedNextTurretPos.Rotation().Radians().value());
 
     params.lookAheadTargetPos = lookAheadTargetPos;
 
